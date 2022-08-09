1dd9564d31f6c5c75b85a2a542501fadb8fc4801
IshanSandhu <ishan.sandhu@mail.mcgill.ca>
2022-08-09 15:43:15 -0500

    Added Form component

--- frontend/resume-parser/src/App.js
+++ frontend/resume-parser/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Form from './pages/form'
+
+import Form from './pages/Form'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path="/">
-        <Form />
-      </Route>
-    </Routes>
-  </Router>
+      <Routes>
+      <Route path="/" element={<Form/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </Router>
   );
 }
 
