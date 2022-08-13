20f97a4a87979ccf856c638b9b50d1ffd1673cdd
Sihui He <sardarbinarytree@gmail.com>
2022-08-12 21:14:20 -0400

    Update Posts.js

--- frontend/resume-parser/src/components/Posts/Posts.js
+++ frontend/resume-parser/src/components/Posts/Posts.js
@@ -6,26 +6,6 @@ const Posts = ({candidates}) => {
     // console.log(candidates);
 
     return (
-            // <Grid
-            //     marginTop='1px'
-            //     display='flex'
-            //     alignItems='center'
-            //     container
-            //     direction="row"
-            //     spacing={5}>
-            //     <Grid item xs={12} sm={6} md={5}>
-            //         {
-            //             candidates.map(
-            //                 (candidate, index) =>
-            //                     (   <Grid key={candidate._id} item md={6} >
-            //                           <Post candidate={candidate} key={index}/>
-            //                         </Grid>
-            //                         )
-            //
-            //             )
-            //         }
-            //     </Grid>
-            // </Grid>
                 <Grid container alignItems="stretch" spacing={4}>
                     {candidates.map((candidate) => (
                         <Grid key={candidate._id} item xs={12} sm={12} md={6} lg={4}>
